a63b1c3e61d50af5b8d1d8b8aa3d1e022aa9d8e4
alina1303 <7759682@gmail.com>
2026-02-18 22:46:39 +0300

    msg changed

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -5,13 +5,13 @@ package guru.qa;
 public class Main {
 
 
-     private static String msg = "Hello qa_guru!";
+     private static String msg = "Hello new qa_guru!";
 
     public static void main(String... args) {
 
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf(getMassege());
+        System.out.printf(getMassege() + "!!");
 
     }
 

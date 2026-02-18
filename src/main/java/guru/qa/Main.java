234c9f76c658fa71033dbfd896e87323761b74f6
akandarova <99600460+akandarova@users.noreply.github.com>
2026-02-18 22:30:55 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -5,7 +5,7 @@ package guru.qa;
 public class Main {
 
      private static String msg = "Hello qa_guru!";
-    public static void main(String[] args) {
+    public static void main(String... args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf(msg);
